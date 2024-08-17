b23669e42ac67cc1db7189f54bbade856ccb27f0
vamshidhar <kasulavamshidhar8@gmail.com>
2024-08-18 01:27:01 +0530

    included swagger documentation

--- src/main/java/com/vamshidhar/cms/advices/GlobalResponseHandler.java
+++ src/main/java/com/vamshidhar/cms/advices/GlobalResponseHandler.java
@@ -9,24 +9,37 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
-public class GlobalResponseHandler  implements ResponseBodyAdvice<Object>{
+public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
 
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter arg1, MediaType arg2,
-            Class<? extends HttpMessageConverter<?>> arg3, ServerHttpRequest arg4, ServerHttpResponse arg5) {
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter arg1,
+            MediaType arg2,
+            Class<? extends HttpMessageConverter<?>> arg3,
+            ServerHttpRequest arg4,
+            ServerHttpResponse arg5) {
 
+        // Avoid wrapping byte arrays and other raw response types
+        if (body instanceof byte[] || arg2.equals(MediaType.APPLICATION_OCTET_STREAM)) {
+            return body;
+        }
 
-        if(body instanceof ApiResponse<?>){
+        // Exclude Swagger endpoints from being wrapped
+        String path = arg4.getURI().getPath();
+        if (path.contains("swagger") || path.contains("api-docs")) {
+            return body;
+        }
+
+        if (body instanceof ApiResponse<?>) {
             return body;
         }
         return new ApiResponse<>(body);
-
     }
 
     @Override
-    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+    public boolean supports(
+            MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
     }
-
-
 }
